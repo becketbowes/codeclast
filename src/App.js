1ff140412129b20525c60f985f3735e981b5933e
becket <becket.bowes@gmail.com>
2022-05-02 00:32:15 -0400

    contact form made

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Nav from './components/Nav';
 import Footer from './components/Footer';
 import About from './components/About';
 import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
 
 function App() {
   return (
     <div>
       <Nav></Nav>
+      <Contact></Contact>
       <About></About>
       <Portfolio></Portfolio>
       <Footer></Footer>
